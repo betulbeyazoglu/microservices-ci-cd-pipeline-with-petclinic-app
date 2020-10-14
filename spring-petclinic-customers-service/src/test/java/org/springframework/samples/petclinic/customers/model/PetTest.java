c28bf4b8d1e6b0fb5eb4b8cb78c307e136c336b9
Betul <betul.nalan@gmail.com>
2020-10-13 21:05:46 -0400

    updated PetTest.java

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
@@ -17,7 +17,7 @@ public class PetTest {
         //Arrange
         Pet pet = new Pet();
         Owner owner = new Owner();
-        owner.setFirstName("Call");
+        owner.setFirstName("Betul");
         //Act
         pet.setOwner(owner);
         //Assert
